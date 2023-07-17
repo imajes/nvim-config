fe82f19c3f0116cc4d82cc7472c852c7b871502d
James Cox <james@imaj.es>
2023-07-17 16:17:30 -0500

    Update neovide config

--- init.lua
+++ init.lua
@@ -1,39 +1,10 @@
--- https://neovide.dev/configuration.html
+-- when using neovide, set gui options
 if vim.g.neovide then
-  vim.o.guifont = "SauceCodePro NF Light:h18:#h-slight:#e-subpixelantialias"
-  vim.g.neovide_scale_factor = 1.0
-  vim.g.neovide_padding_top = 5
-  vim.g.neovide_padding_bottom = 5
-  vim.g.neovide_padding_right = 5
-  vim.g.neovide_padding_left = -5
-
-  vim.g.neovide_remember_window_size = true
-  vim.g.neovide_remember_window_position = true
-
-  -- dark/light mode
-  vim.g.neovide_theme = "auto"
-
-  -- vim.g.neovide_cursor_vfx_mode = "railgun"
-
-  -- this is the anim which shows on buffer load etc; keep it short because it's odd
-  vim.g.neovide_position_animation_length = 0
-  vim.g.neovide_scroll_animation_length = 0.1
-  vim.g.neovide_cursor_animation_length = 0.01
-
-  vim.g.neovide_refresh_rate = 60
-  vim.g.neovide_profiler = true
-
-  -- Helper function for transparency formatting
-  local alpha = function()
-    return string.format("%x", math.floor(255 * vim.g.transparency))
-  end
-
-  -- g:neovide_transparency should be 0 if you want to unify transparency of content and title bar.
-  -- vim.g.neovide_transparency = 0.0
-  -- vim.g.transparency = 0.9
-  -- vim.g.neovide_background_color = "#0f1117" .. alpha()
+  require("config.neovide")
 end
 
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 require("config.folds")
+
+-- vim.lsp.set_log_level("debug")
---
+++ lua/config/neovide.lua
@@ -0,0 +1,70 @@
+-- https://neovide.dev/configuration.html
+--
+-- vim.o.guifont = "SauceCodePro NF Light:h18:#h-slight:#e-subpixelantialias"
+vim.o.guifont = "Fira Code Light:h18:#h-slight:#e-subpixelantialias"
+
+-- https://github.com/tonsky/FiraCode/wiki/How-to-enable-stylistic-sets
+
+-- vim.g.neovide.font_defs = {
+--   { "code_body",
+--     {
+--       family = "Fira Code",
+--       weight = "Light",
+--       size = 18,
+--       bold = false,
+--       italic = false,
+--       edging = "subpixelantialias",
+--       hinting = "slight",
+--       features = { "zero", "cv02", "cv01", "ss01", "cv14", "ss08", "cv24", "ss06", "cv27", "ss07" },
+--     }
+--   },
+--   { "buffer_tab_title",
+--     {
+--       family = "Fira Code",
+--       weight = "Bold",
+--       size = 16,
+--       bold = true,
+--       italic = false,
+--       edging = "subpixelantialias",
+--       hinting = "slight",
+--       features = { "zero", "cv02", "cv01", "ss01", "cv14", "ss08", "cv24", "ss06", "cv27", "ss07" },
+--     }
+--   },
+-- }
+-- vim.o.guifont = "neovide_font:code_body,neovide_font:buffer_tab_title,fallback_font_normal_def"
+--
+--
+-- vim.g.neovide_font_features = { "zero", "cv02", "cv01", "ss01", "cv14", "ss08", "cv24", "ss06", "cv27", "ss07" },
+-- a g 0 /= <= >= != -> <- => := ... ++ -- :: // ** /* */ << >> && || ?? ::= \\ [] {. .} =~
+
+vim.g.neovide_scale_factor = 1.0
+vim.g.neovide_padding_top = 5
+vim.g.neovide_padding_bottom = 5
+vim.g.neovide_padding_right = 5
+vim.g.neovide_padding_left = -5
+
+vim.g.neovide_remember_window_size = true
+vim.g.neovide_remember_window_position = true
+
+-- dark/light mode
+vim.g.neovide_theme = "auto"
+
+-- vim.g.neovide_cursor_vfx_mode = "railgun"
+
+-- this is the anim which shows on buffer load etc; keep it short because it's odd
+vim.g.neovide_position_animation_length = 0
+vim.g.neovide_scroll_animation_length = 0.1
+vim.g.neovide_cursor_animation_length = 0.01
+
+vim.g.neovide_refresh_rate = 60
+-- vim.g.neovide_profiler = true
+
+-- Helper function for transparency formatting
+local alpha = function()
+  return string.format("%x", math.floor(255 * vim.g.transparency))
+end
+
+-- g:neovide_transparency should be 0 if you want to unify transparency of content and title bar.
+-- vim.g.neovide_transparency = 0.0
+-- vim.g.transparency = 0.9
+-- vim.g.neovide_background_color = "#0f1117" .. alpha()
