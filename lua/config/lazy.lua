5d58966f581388f8f2a7f8e6ea625e55f9f5f8cf
James Cox <james@imaj.es>
2023-07-28 12:39:13 -0500

    update lazyvim plugin configs

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -16,22 +16,28 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = { colorscheme = vim.g.preferred_colorscheme } },
-    { "folke/noice.nvim", cond = not vim.g.neovide },
+    { "folke/noice.nvim" }, --, cond = not vim.g.neovide },
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.coding.yanky" },
     { import = "lazyvim.plugins.extras.dap.core" },
+    { import = "lazyvim.plugins.extras.dap.nlua" },
+    { import = "lazyvim.plugins.extras.lang.cmake" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.elixir" },
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.ruby" },
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.terraform" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.ui.mini-animate", cond = not vim.g.neovide },
+    { import = "lazyvim.plugins.extras.util.mini-hipatterns", cond = not vim.g.neovide },
+    { import = "lazyvim.plugins.extras.util.project" },
     -- import/override with your plugins
     { import = "plugins" },
     -- specific [non-glob] files in local plugins
