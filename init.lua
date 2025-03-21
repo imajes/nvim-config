653c8a0a6dd329314ee11a1125557a52bfc4ffbc
James Cox <james@imaj.es>
2025-03-21 17:06:47 -0500

    [FOLDING] Tweak folding rules, possibly need to revert.

--- init.lua
+++ init.lua
@@ -5,6 +5,11 @@ end
 
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-require("config.folds")
 
--- vim.lsp.set_log_level("debug")
+-- set the lsp log level to trace for lsp
+-- vim.lsp.set_log_level("trace")
+-- require("vim.lsp.log").set_format_func(vim.inspect)
+
+-- vim.print("cache stdpath..")
+-- vim.print(vim.fn.stdpath("cache"))
+vim.env.UFO_LOG = "TRACE"
