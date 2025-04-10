7cb013bac0dcf285d52a48a068028b2c6f267936
James Cox <james@imaj.es>
2025-04-10 14:16:15 -0500

    Tweak neovide settings to improve cursor etc

--- lua/config/neovide.lua
+++ lua/config/neovide.lua
@@ -12,22 +12,30 @@ vim.g.neovide_remember_window_position = true
 -- dark/light mode
 -- vim.g.neovide_theme = "auto"
 
--- vim.g.neovide_cursor_vfx_mode = "railgun"
+-- cursor configuration
+-- https://neovide.dev/configuration.html?highlight=trail#cursor-settings
+vim.g.neovide_cursor_antialiasing = true
+vim.g.neovide_cursor_animate_command_line = true
+vim.g.neovide_cursor_unfocused_outline_width = 0.125
+vim.g.neovide_cursor_trail_size = 0.25 -- how long the wierd trail is
+vim.g.neovide_cursor_vfx_mode = "pixiedust"
+vim.g.neovide_cursor_vfx_opacity = 50.0
+vim.g.neovide_cursor_vfx_particle_lifetime = 2.5
+vim.g.neovide_cursor_vfx_particle_density = 25.0
+vim.g.neovide_cursor_vfx_particle_speed = 75.0
 
 -- this is the anim which shows on buffer load etc; keep it short because it's odd
-vim.g.neovide_position_animation_length = 0
-vim.g.neovide_scroll_animation_length = 0.1
-vim.g.neovide_cursor_animation_length = 0.01
+-- vim.g.neovide_position_animation_length = 0
+-- vim.g.neovide_scroll_animation_length = 0.1
+-- vim.g.neovide_cursor_animation_length = 0.01
 
 vim.g.neovide_refresh_rate = 120
+
+-- opacity/transparency settings
+vim.g.neovide_opacity = 0.8
+vim.g.neovide_normal_opacity = 0.8
+
+-- window blur?
+vim.g.neovide_window_blurred = true
+
 -- vim.g.neovide_profiler = true
-
--- Helper function for transparency formatting
-local alpha = function()
-  return string.format("%x", math.floor(255 * vim.g.transparency))
-end
-
--- g:neovide_transparency should be 0 if you want to unify transparency of content and title bar.
--- vim.g.neovide_transparency = 0.0
--- vim.g.transparency = 0.9
--- vim.g.neovide_background_color = "#0f1117" .. alpha()
