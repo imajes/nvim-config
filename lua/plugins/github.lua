8c11157a4a00c3db88b63ae2cfba80505848515d
James Cox <james@imaj.es>
2025-04-10 14:18:23 -0500

    Add functionality to post direct to gist

---
+++ lua/plugins/github.lua
@@ -0,0 +1,27 @@
+-- Section: CODE, Github Integrations
+-- ---------------------------------
+
+return {
+  {
+    "Rawnly/gist.nvim",
+    cmd = { "GistCreate", "GistCreateFromFile", "GistsList" },
+    config = true,
+  },
+  -- `GistsList` opens the selected gif in a terminal buffer,
+  -- nvim-unception uses neovim remote rpc functionality to open the gist in an actual buffer
+  -- and prevents neovim buffer inception
+  {
+    "samjwill/nvim-unception",
+    lazy = false,
+    init = function()
+      vim.g.unception_block_while_host_edits = true
+    end,
+  },
+  -- -- update the model used for gh copilot
+  -- {
+  --   "CopilotC-Nvim/CopilotChat.nvim",
+  --   opts = {
+  --     model = "gpt-4o",
+  --   },
+  -- },
+}
