43d4e85c9ef958f1f302dfe10e80b5e418a2427f
James Cox <james@imaj.es>
2025-04-10 14:18:37 -0500

    Add debug statements

---
+++ lua/config/debug.lua
@@ -0,0 +1,7 @@
+-- set the lsp log level to trace for lsp
+-- vim.lsp.set_log_level("trace")
+-- require("vim.lsp.log").set_format_func(vim.inspect)
+
+-- vim.print("cache stdpath..")
+-- vim.print(vim.fn.stdpath("cache"))
+vim.env.UFO_LOG = "TRACE"
